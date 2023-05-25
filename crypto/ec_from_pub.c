44c44003d274aba1aac53a05f2afe0113c9ef51e
Rock-Bautumn <3623@holbertonschool.com>
2023-05-25 11:07:30 -0500

    Adding check for NULL public key for task 3, ec_from_pub()

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -11,6 +11,9 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 	EC_POINT *ec_point;
 	EC_KEY *ec_key;
 
+	if (!pub)
+		return (NULL);
+
 	ec_key = EC_KEY_new_by_curve_name(EC_CURVE);
 	ec_group = EC_KEY_get0_group(ec_key);
 	ec_point = EC_POINT_new(ec_group);
