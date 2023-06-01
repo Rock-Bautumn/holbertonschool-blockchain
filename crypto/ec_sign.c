a8d787a4b4e72fbd6dc041018d9c7f240fd4566d
Rock-Bautumn <3623@holbertonschool.com>
2023-05-31 21:32:06 -0500

    Adding task 6, ec_sign.c

---
+++ crypto/ec_sign.c
@@ -0,0 +1,32 @@
+#include "hblk_crypto.h"
+
+/**
+ * ec_sign - Sign a given set of bytes, using a given EC_KEY private key,
+ * into the given sig_t structure
+ * @key: The EC_KEY object containing the private key
+ * @msg: The value of the hash value (the given bytes) to sign
+ * @msglen: The length of the digest hash value
+ * @sig: The signature structure that stores the signature and its length
+ * Return: The signature that is contained in the given sig_t signature struct
+*/
+
+uint8_t *ec_sign(
+	EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t *sig)
+{
+	int32_t sig_len = 0;
+
+	if (!key || !msg || !sig)
+		return (NULL);
+
+	memset(sig->sig, '\0', sizeof(sig->sig));
+
+	if (!ECDSA_sign(0, msg, msglen, sig->sig, &sig_len, (EC_KEY *)key))
+	{
+		sig->len = 0;
+		return (NULL);
+	}
+
+	sig->len = (uint8_t)sig_len;
+
+	return (sig->sig);
+}
