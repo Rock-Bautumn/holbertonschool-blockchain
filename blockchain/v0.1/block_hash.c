88ec3531f00625fe41c4922d3200dd11f60c99bd
Rock-Bautumn <3623@holbertonschool.com>
2023-06-12 21:10:55 -0500

    Returning the hash buffer instead for block_hash.c

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -7,12 +7,17 @@
  * Return: The address of the saved resulting hash
 */
 
-uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+uint8_t *block_hash(
+	block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
 	if (!block)
 		return (NULL);
 
-	return (sha256((int8_t const *) block,
+	sha256(
+		(int8_t const *) block,
 		sizeof(block->info) + block->data.len,
-		hash_buf));
+		hash_buf
+	)
+
+	return (hash_buf);
 }
