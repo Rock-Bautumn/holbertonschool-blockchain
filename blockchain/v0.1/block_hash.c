d5367f23f090dc95f45eb78794bbac9caf01685d
Rock-Bautumn <3623@holbertonschool.com>
2023-06-12 21:08:29 -0500

    Doing a light refactor of block_hash.c

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -9,11 +9,10 @@
 
 uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	size_t size_to_hash;
-
 	if (!block)
 		return (NULL);
-	
-	size_to_hash = sizeof(block->info) + block->data.len;
-	return (sha256((int8_t const *) block, size_to_hash, hash_buf));
+
+	return (sha256((int8_t const *) block,
+		sizeof(block->info) + block->data.len,
+		hash_buf));
 }
