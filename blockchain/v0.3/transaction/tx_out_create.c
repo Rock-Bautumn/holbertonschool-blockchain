d0ba00407daf642c650719e92adff39ac1efc59d
Rock-Bautumn <3623@holbertonschool.com>
2023-07-17 21:21:55 -0500

    Adding Betty docs

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -1,5 +1,12 @@
 #include "../blockchain.h"
 
+/**
+ * tx_out_create - Create a tx_out data structure
+ * @amount: The amount of the transaction
+ * @pub: The public key of the transaction recipient
+ * Return: The address of the created transaction data structure, NULL if fail
+*/
+
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
 {
 	tx_out_t *tx_change;
@@ -11,7 +18,7 @@ tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
 	tx_change->amount = amount;
 	memcpy(tx_change->pub, pub, EC_PUB_LEN);
 	SHA256((const unsigned char *)tx_change,
-		4 + EC_PUB_LEN,
+		sizeof(tx_change->amount) + EC_PUB_LEN,
 		tx_change->hash);
 
 	return (tx_change);
